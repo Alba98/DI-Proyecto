3e8fbac7d325d747f914b6337eb531e47f789228
Alba <alba.alonso@ikasle.egibide.org>
2022-12-20 20:26:29 +0100

    login conexion con BBDD

--- Proyecto/Proyecto/Controlador/Controlador.vb
+++ Proyecto/Proyecto/Controlador/Controlador.vb
@@ -1,24 +1,67 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
 
 Public Class Controlador
-    'Shared SQLConnection As New MySqlConnection("DataSource=localhost;port=8000;Database=db1;Username=test;Password=test;")
-    'Shared SQLConnection As New MySqlConnection("server=localhost;port=8000;userid=db1;database=db1;Pwd=test;")
-    Shared connStr As String = "server=localhost;user=db1;database=db1;port=8000;password=test"
-    Shared SQLConnection = New MySqlConnection(connStr)
+    Shared SQLConnection As MySqlConnection
     Shared dt As New DataTable
     Shared ireturn As Boolean
 
-    Friend Shared Sub login(usuario As String, clave As String)
-        If (usuario = "aalonso") Then
-            If (clave = "12345") Then
-                App.Show()
-                LoginForm.Close()
-            End If
-        End If
+    Friend Shared Sub ConectarBBDD()
+        Try
+            ' Ejecutan las instrucciones dento del Try
+            ' En caso que alguna de ellas se ejecute con un error, entonces, se pasa automáticamente a la sección "Catch" que es quien atrapa el error
+
+            ' Crear un objeto de tipo ConnectionString
+            Dim vConnString As New MySqlConnectionStringBuilder
+            vConnString.Server = "localhost"
+            vConnString.Database = "db1"
+            vConnString.UserID = "db1"
+            vConnString.Password = "test"
+            vConnString.IntegratedSecurity = False
+
+            ' Usar ese objeto para conectarnos
+            SQLConnection = New MySqlConnection(vConnString.ToString)
+            SQLConnection.Open()
+
+            MsgBox("Conexión establecida correctamente")
+
+        Catch ex As MySqlException
+            MsgBox("Error al conectarse a la BD", MsgBoxStyle.Critical)
+        End Try
     End Sub
 
-    Friend Shared Function Insertar() As Boolean
+    Friend Shared Function login(usuario As String, clave As String) As Boolean
 
+        Dim sql As String = "SELECT clave FROM usuarios WHERE usuario = @usuario"
+        Dim cmd As New MySqlCommand(sql, SQLConnection)
+        cmd.Parameters.AddWithValue("@usuario", usuario)
+
+        Dim rd As MySqlDataReader = cmd.ExecuteReader
+        Dim claveBD As String
+        If rd.Read Then
+            claveBD = rd.GetString(0)
+            If (clave = claveBD) Then
+                'App.Show()
+                'LoginForm.Close()
+                rd.Close()
+                Return True
+            End If
+        Else
+            rd.Close()
+            Return False
+        End If
+
+
+
+        'If (usuario = "aalonso") Then
+        '    If (clave = "12345") Then
+        '        App.Show()
+        '        LoginForm.Close()
+        '    End If
+        'End If
+    End Function
+
+    Friend Shared Function insertar() As Boolean
         Dim sql As String = "INSERT INTO usuarios (usuario, nombre, apellidos, email, contrasena) VALUES (@usuario, @nombre, @apellidos, @email, @contrasena)"
 
         Dim cmd = New MySqlCommand(sql, SQLConnection)
@@ -30,18 +73,22 @@ Public Class Controlador
         cmd.Parameters.AddWithValue("@contrasena", "12345")
 
         Try
-            SQLConnection.Open()
+            'SQLConnection.Open()
             If cmd.ExecuteNonQuery() > 0 Then
                 ireturn = True
             End If
-            SQLConnection.Close()
+            'SQLConnection.Close()
 
 
         Catch ex As Exception
             ireturn = False
-            SQLConnection.Close()
+            'SQLConnection.Close()
         End Try
 
         Return ireturn
     End Function
+
+    Shared vConn As MySqlConnection
+
+
 End Class
--- Proyecto/Proyecto/Vista/LoginForm.vb
+++ Proyecto/Proyecto/Vista/LoginForm.vb
@@ -1,15 +1,19 @@
 ﻿Public Class LoginForm
+    Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Controlador.ConectarBBDD()
+        tUsuario.Text = "aalonso"
+        tClave.Text = "12345"
+    End Sub
+
     Private Sub cbClave_CheckedChanged(sender As Object, e As EventArgs) Handles cbClave.CheckedChanged
-        'If (cbClave.Checked) Then
-        '    tClave.PasswordChar = ""
-        'End If
-
         tClave.PasswordChar = If(cbClave.Checked, "", "*")
-
     End Sub
 
     Private Sub bAceptar_Click(sender As Object, e As EventArgs) Handles bAceptar.Click
-        Controlador.login(tUsuario.Text, tClave.Text)
+        If Controlador.login(tUsuario.Text, tClave.Text) Then
+            App.Show()
+            Me.Close()
+        End If
     End Sub
 
     Private Sub bSalir_Click(sender As Object, e As EventArgs) Handles bSalir.Click
